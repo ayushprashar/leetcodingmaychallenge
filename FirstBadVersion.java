440b8c0a3b13e0c13ced682d252ca37f13e11848
Ayush <ayush.prashar@gmail.com>
2020-05-01 15:20:12 +0530

    Final version submitted

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -1,21 +1,14 @@
 public class FirstBadVersion extends VersionControl {
     public int firstBadVersion(int n) {
-            return innerFBV(1,n);
-    }
-    private int innerFBV(int start, int end) {
-        int mid = (start + end)/2;
-        boolean isBad = isBadVersion(mid);
-        if(isBad) {
-            if(mid - 1 > 0 && !isBadVersion(mid-1))
-                return mid;
-            if(mid == 1)
-                return 1;
-            return innerFBV(start, mid -1);
-        } else {
-            if(mid + 1<=end && isBadVersion(mid+1)) {
-                return mid+1;
-            } else
-                return innerFBV(mid+1, end);
+        int start = 1;
+        int end = n;
+        while(start < end) {    
+            int mid = start + (end - start)/2;
+            if(isBadVersion(mid))
+                end = mid;
+            else 
+                start = mid + 1;
         }
+        return start;   
     }
 }
